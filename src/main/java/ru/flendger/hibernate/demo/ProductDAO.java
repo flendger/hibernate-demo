0d66f85d7fc2b005750707bd970f6206724b3b74
flendger <kiru.ee@yandex.ru>
2020-12-19 18:58:48 +0300

    Урок 5: - добавлены зависимости в pom.xml - реализован класс Product - реализован класс ProductDAO - реализован запуск приложения фабрика сессий создается один раз в основном приложении, и потом передает в DAO для генерации сессий и транзакций.

---
+++ src/main/java/ru/flendger/hibernate/demo/ProductDAO.java
@@ -0,0 +1,68 @@
+package ru.flendger.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class ProductDAO {
+
+    private final SessionFactory factory;
+    private Session session;
+    private Transaction transaction;
+
+    public ProductDAO(SessionFactory factory) {
+        this.factory = factory;
+    }
+
+    public Session openSession() {
+        session = factory.openSession();
+        return session;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void closeSession() {
+        session.close();
+    }
+
+    public Transaction openTransaction() {
+        transaction = session.beginTransaction();
+        return transaction;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public void closeTransaction() {
+        transaction.commit();
+        session.close();
+    }
+
+    public Product findById(Long id) {
+        Product product = session.get(Product.class, id);
+        return product;
+    }
+
+    public List<Product> findAll() {
+        return null;
+    }
+
+    public Product saveOrUpdate(Product product) {
+        getSession().saveOrUpdate(product);
+        return product;
+    }
+
+    public void deleteById(Long id) {
+        Product product = findById(id);
+        if (product == null) {
+            throw new NoSuchElementException(String.valueOf(id));
+        }
+        getSession().delete(product);
+    }
+}
